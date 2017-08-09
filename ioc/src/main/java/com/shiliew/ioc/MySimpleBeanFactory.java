aab0959a7e172e10d6961df098ae040c9570813f
Shiliew <704091119@qq.com>
2017-08-09 17:27:49 +0800

    update

--- ioc/src/main/java/com/shiliew/ioc/MySimpleBeanFactory.java
+++ ioc/src/main/java/com/shiliew/ioc/MySimpleBeanFactory.java
@@ -1,7 +1,7 @@
 package com.shiliew.ioc;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
 /**
@@ -10,9 +10,20 @@ import org.springframework.core.io.ClassPathResource;
 public class MySimpleBeanFactory {
 
     public static void main(String [] args){
+        //1.创建bean的抽象资源
         ClassPathResource resource = new ClassPathResource("/ioc/user.xml");
-        BeanFactory beanfactory = new XmlBeanFactory(resource);
-        User user = beanfactory.getBean("user", User.class);
+
+        //2.场景bean工厂
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        //3.创建bean定义读取器
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        //4.回调返回给bean工厂并解析
+        reader.loadBeanDefinitions(resource);
+          //1步完成第2、3、4步
+//        BeanFactory factory = new XmlBeanFactory(resource);
+
+        //5.注册bean
+        User user = factory.getBean("user", User.class);
         System.out.println(user.getName() + " " + user.getAge());
     }
 }
